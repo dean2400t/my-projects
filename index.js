fe7bf3bed205e3d42465ce1ecd7586167da665d0
dean2400t <dean2400t@gmail.com>
2021-06-01 10:15:05 +0300

    הוספות ותיקונים

---
+++ index.js
@@ -0,0 +1,45 @@
+let prevColsInRow;
+function addRowClass(){
+    let colsInRow;
+    if (window.innerWidth<960)
+        colsInRow=1;
+    else if (window.innerWidth<1140)
+        colsInRow=2;
+    else
+        colsInRow=3;
+    
+    if (prevColsInRow!==colsInRow)
+    {
+        const projects = document.getElementsByClassName("project");
+        let projectNumber=0;
+        for(var i = 0; i < projects.length; i++)
+        {
+            const project=projects[i];
+            if (prevColsInRow){
+                const prevRow=$(project).attr('data-projNum');
+                $(project).removeClass('rowNum'+prevRow);
+            }
+            const newRow=parseInt(parseInt(projectNumber)/colsInRow)+1;
+            $(project).attr('data-projNum', newRow);
+            $(project).addClass('rowNum'+newRow);
+            projectNumber++;
+        }
+        prevColsInRow=colsInRow;
+    }
+}
+
+function mouseOverDemo(idOfProject){
+    addRowClass();
+    const rowNum=$('#'+idOfProject).attr("data-projNum");
+    $('.demoPass:not(#'+idOfProject+')').removeClass('demoPass');
+    $('.demoPassHidden.rowNum'+rowNum+':not(#'+idOfProject+')').addClass('demoPassBlank');
+    $('#'+idOfProject).addClass('demoPass');
+}
+
+
+
+function mouseOutCard(){
+    $('.demoPass').removeClass('demoPass');
+    $('.demoPassHidden').removeClass('demoPassBlank');
+}
+
